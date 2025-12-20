498acaf229e6586db840b97eba0d02a88f5a4241
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2025-12-20 13:26:00 +0300

    Изменена конфигурация

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sass from 'sass';
+import node from '@astrojs/node';
 
 export default defineConfig({
   site: 'https://rus-news.online',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   markdown: {
     // Настройки для Markdown по умолчанию
   },
